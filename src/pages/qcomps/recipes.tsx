6fe52f32e8c9aff4f224d266062c82e75f60650a
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2025-01-25 20:44:21 -0500

    Modified recipes.tsx to display an unordered list of ingredients for every dish in the "recipes" object

--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -12,11 +12,26 @@ export const recipes = [{
   ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
+function getIngredients(ingredients: Set<string>) {
+  const list: JSX.Element[] = [];
+  ingredients.forEach(value =>
+      list.push(
+          <li key={value}>{value}</li>
+      ));
+  return list;
+}
+
 export default function RecipeList() {
+  const listItems = recipes.map(recipe =>
+      <li key={recipe.id}>
+      <h2>{recipe.name}</h2>
+        <ul>{getIngredients(recipe.ingredients)}</ul>
+      </li>
+  );
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {listItems}
     </div>
   );
 }
