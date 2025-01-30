c03ed3f4fc36ba496d8722d55ac1af13499ed58d
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2025-01-30 10:57:41 -0500

    Fixed list out of bounds error in qcomps/state.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Gallery from "@/pages/qcomps/state";
 
 export default function Home() {
   return (
-    <Profile />
+    //<Profile />
+      <Gallery/>
   )
 }
